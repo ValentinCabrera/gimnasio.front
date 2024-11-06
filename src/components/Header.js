4a96f88cefad93833adebff0f53d300fb7d132c2
FeliBlanco <felipeblanco09@hotmail.com>
2024-11-06 12:15:45 -0300

    Cambio menú header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,36 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, Button, List, ListItem, ListItemText, Toolbar, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+
+import Logo from '../imgs/logo.png'
 
 export default function Header() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        height: "64px",
-        width: "100%",
-        backgroundColor: "#ffe600",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "0 16px",
-        boxSizing: "border-box",
-        zIndex: 1000,
-      }}
-    >
-      <Box
-        sx={{
-          flex: "0 0 auto",
-          marginRight: "20px",
-        }}
-      >
-        <Sidebar />
-      </Box>
-    </div>
+      <AppBar sx={{background:'#de0202', padding:{xs:'2px', md:'10px'}}}>
+        <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
+          <Box sx={{display:'flex', alignItems:'center', flex:1}}>
+            <Box sx={{width:'80px'}}>
+              <img src={Logo} style={{width:'100%'}}/>
+            </Box>
+            <Typography fontSize={'22px'} fontWeight={'bold'}>CENTRAL GYM</Typography>
+          </Box>
+          <Box sx={{flex:1, display:{xs:'none', md:'block'}}}>
+            <Box sx={{display:'flex', alignItems:'center', gap:'20px', justifyContent:'center'}}>
+              <Button sx={{color:'#fff', fontSize:'24px'}}>
+                <Typography fontSize={'22px'} fontWeight={'bold'}>Inicio</Typography>
+              </Button>
+              <Button sx={{color:'#fff'}}>
+                <Typography fontSize={'22px'} fontWeight={'bold'}>Contacto</Typography>
+              </Button>
+              <Button sx={{color:'#fff'}}>
+                <Typography fontSize={'22px'} fontWeight={'bold'}>Mis planes</Typography>
+              </Button>
+            </Box>
+          </Box>
+          <Box sx={{flex:1, display:'flex', justifyContent:'flex-end'}}>
+            <Button><Typography fontSize={'22px'} sx={{color:'#fff'}}>Iniciar sesión</Typography></Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      
   );
 }
